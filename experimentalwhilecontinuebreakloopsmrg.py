07e062d1fba493f4035e6ad5faa2438f03512eac
MRGonzalez1990 <maurogonzalezplus@gmail.com>
2024-05-22 13:48:45 -0700

    Add files via upload

---
+++ experimentalwhilecontinuebreakloopsmrg.py
@@ -0,0 +1,13 @@
+#experimenting while continue break loops
+y=int(input('What is your age?'))
+while True:
+    y+=(y)
+    if y==y:
+        print ('Nice age.')
+        break
+x=input('What is your name?')
+while True:
+    x+=(x)
+    if x==x:
+        print ('Nice name.')
+        break
